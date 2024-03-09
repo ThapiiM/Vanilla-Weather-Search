c12e206395c4762090b72570c88a27bac2781d3e
ThapiiM <125468879+ThapiiM@users.noreply.github.com>
2024-03-09 10:27:41 +0200

    Added the searchEngine function in order to activate the form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function searchEngine(event) {
+  event.preventDefault();
 
+  /*Assigning this action to activate the input field */
+  let searchForm = document.querySelector(".search-input");
+
+  /*Assigning this action to activating the transition on the h1 */
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchForm.value;
+}
+
+let searchFormElement = document.querySelector(".search-form"); //Targets to activate the entire form
+searchFormElement.addEventListener("submit", searchEngine);
